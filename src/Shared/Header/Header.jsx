ff41999862e40f75df9d70a2a65a4ca333a1f3cb
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2025-09-22 02:31:52 +0600

    Hero section Done

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -4,7 +4,16 @@ import vdoBg from "../../assets/134998-760679973_small.mp4";
 const Header = () => {
   return (
     <div className="video-content">
+      <div className="overlay"></div>
       <video src={vdoBg} autoPlay loop muted />
+      <div className="video-text">
+        <h1 className="text-6xl font-bold text-white text-center">
+          Tour লাগবে?
+        </h1>
+        <p className="text-2xl text-white text-center mt-4">
+          We provide the best services for you.
+        </p>
+      </div>
     </div>
   );
 };
